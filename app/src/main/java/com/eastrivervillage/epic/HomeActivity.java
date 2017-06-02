791617fdc77fbcb40951cbbd641ac2c324f0426b
Balakumaran <kumaran.4353@gmail.com>
2017-06-02 08:52:28 +0530

    Temp: June-2

--- app/src/main/java/com/eastrivervillage/epic/HomeActivity.java
+++ app/src/main/java/com/eastrivervillage/epic/HomeActivity.java
@@ -1,11 +1,15 @@
 package com.eastrivervillage.epic;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +20,22 @@ public class HomeActivity extends AppCompatActivity {
         ((TextView) findViewById(R.id.tv_daily_images)).setTypeface(font);
         ((TextView) findViewById(R.id.tv_special_events)).setTypeface(font);
         ((TextView) findViewById(R.id.tv_discover)).setTypeface(font);
+
+        ((LinearLayout) findViewById(R.id.ll_daily_images)).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = null;
+
+        switch (v.getId()) {
+            case R.id.ll_daily_images:
+                intent = new Intent(HomeActivity.this, MainActivity.class);
+                break;
+        }
+
+        if (null != intent) {
+            startActivity(intent);
+        }
     }
 }
