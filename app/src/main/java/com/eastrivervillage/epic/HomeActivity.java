fde40324aae67633239f11f78b7763419c28d1e3
Balakumaran <kumaran.4353@gmail.com>
2017-06-04 23:19:39 +0530

    Permission check moved to HomeActivity

--- app/src/main/java/com/eastrivervillage/epic/HomeActivity.java
+++ app/src/main/java/com/eastrivervillage/epic/HomeActivity.java
@@ -1,8 +1,16 @@
 package com.eastrivervillage.epic;
 
+import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Typeface;
+import android.os.SystemClock;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +18,15 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener{
+
+    public static final int ALLOW_INTERNET_DIALOG_CODE = 1;
+    public static final int NO_INTERNET_EXIT_CODE = 2;
+    public static final int ALLOW_INTERNET_STATUS_DIALOG_CODE = 3;
+    public static final int NO_INTERNET_STATUS_EXIT_CODE = 4;
+    public static final int ENABLE_INTERNET_DIALOG_CODE = 5;
+
+    public static final int ALLOW_INTERNET_REQUEST_CODE = 1270;
+    public static final int ALLOW_NETWORK_STATE_REQUEST_CODE = 1271;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +40,8 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
 
         ((LinearLayout) findViewById(R.id.ll_daily_images)).setOnClickListener(this);
         ((LinearLayout) findViewById(R.id.ll_special_events)).setOnClickListener(this);
+
+        checkForInternetPermission();
     }
 
     @Override
@@ -41,5 +60,148 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         if (null != intent) {
             startActivity(intent);
         }
+    }
+
+    public void checkForInternetPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
+            /* Need to provide an explanation */
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.INTERNET)) {
+                showDialog(getString(R.string.permission), getString(R.string.need_Internet_permission),
+                        android.R.drawable.ic_dialog_info, getString(R.string.allow), getString(R.string.deny), ALLOW_INTERNET_DIALOG_CODE);
+            } else {
+                requestInternetPermission();
+            }
+        } else {
+            checkNetworkStatePermission();
+        }
+    }
+
+    public void showDialog(final String title, final String msg, final int icon, final String positiveStr, final String negativeStr,
+                           final int callbackCode) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                new AlertDialog.Builder(HomeActivity.this)
+                        .setTitle(title)
+                        .setMessage(msg)
+                        .setIcon(icon)
+                        .setPositiveButton(positiveStr, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialogCallback(callbackCode, true);
+                            }
+                        })
+                        .setNegativeButton(negativeStr, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialogCallback(callbackCode, false);
+                            }
+                        })
+                        .show();
+            }
+        });
+    }
+
+    public void dialogCallback(int code, boolean success) {
+        switch (code) {
+            case ALLOW_INTERNET_DIALOG_CODE:
+                if (success) {
+                    requestInternetPermission();
+                } else {
+                    showNoInternetExitDialog();
+                }
+                break;
+
+            case ALLOW_INTERNET_STATUS_DIALOG_CODE:
+                if (success) {
+                    requestNetworkStatusPermission();
+                } else {
+                    showNoNetworkStatusExitDialog();
+                }
+                break;
+
+            case NO_INTERNET_EXIT_CODE:
+                if (success) {
+                    checkForInternetPermission();
+                } else {
+                    System.exit(0);
+                }
+                break;
+
+            case NO_INTERNET_STATUS_EXIT_CODE:
+                if (success) {
+                    checkForInternetPermission();
+                } else {
+                    System.exit(0);
+                }
+                break;
+
+            case ENABLE_INTERNET_DIALOG_CODE:
+                if (success) {
+                    startActivityForResult(new Intent(Settings.ACTION_WIFI_SETTINGS), 0);
+                    SystemClock.sleep(3000);
+                } else {
+                    System.exit(0);
+                }
+                break;
+        }
+    }
+
+    public void checkNetworkStatePermission() {
+        if (ContextCompat.checkSelfPermission(HomeActivity.this, Manifest.permission.ACCESS_NETWORK_STATE) !=
+                PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(HomeActivity.this, Manifest.permission.ACCESS_NETWORK_STATE)) {
+                showDialog(getString(R.string.permission), getString(R.string.need_nw_state_permissin),
+                        android.R.drawable.ic_dialog_info, getString(R.string.allow), getString(R.string.deny), ALLOW_INTERNET_STATUS_DIALOG_CODE);
+            } else {
+                requestNetworkStatusPermission();
+            }
+        }
+    }
+
+    public void requestInternetPermission() {
+        ActivityCompat.requestPermissions(HomeActivity.this,
+                new String[]{Manifest.permission.INTERNET},
+                ALLOW_INTERNET_REQUEST_CODE);
+    }
+
+    public void requestNetworkStatusPermission() {
+        ActivityCompat.requestPermissions(HomeActivity.this,
+                new String[]{Manifest.permission.ACCESS_NETWORK_STATE},
+                ALLOW_NETWORK_STATE_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case ALLOW_INTERNET_REQUEST_CODE:
+                if (grantResults.length > 0 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    /* Permission granted for Internet access */
+                    checkNetworkStatePermission();
+                } else {
+                    showNoInternetExitDialog();
+                }
+                break;
+
+            case ALLOW_NETWORK_STATE_REQUEST_CODE:
+                if (grantResults.length > 0 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    /* Permission Granted for Network state */
+                } else {
+                    showNoNetworkStatusExitDialog();
+                }
+                break;
+        }
+    }
+    
+    public void showNoInternetExitDialog() {
+        showDialog(getString(R.string.permission), getString(R.string.no_proceed_without_Internet),
+                android.R.drawable.ic_dialog_alert, getString(R.string.allow), getString(R.string.exit), NO_INTERNET_EXIT_CODE);
+    }
+
+    public void showNoNetworkStatusExitDialog() {
+        showDialog(getString(R.string.permission), getString(R.string.no_proceed_without_nw_access),
+                android.R.drawable.ic_dialog_alert, getString(R.string.allow), getString(R.string.exit), NO_INTERNET_STATUS_EXIT_CODE);
     }
 }
--- app/src/main/java/com/eastrivervillage/epic/MainActivity.java
+++ app/src/main/java/com/eastrivervillage/epic/MainActivity.java
@@ -7,15 +7,9 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.Uri;
-import android.os.SystemClock;
 import android.preference.PreferenceManager;
-import android.provider.Settings;
-import android.support.annotation.NonNull;
-import android.support.v4.app.*;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -41,16 +35,6 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements DetailFragment.OnFragmentInteractionListener, ListFragment.OnFragmentInteractionListener, DatePickerDialog.OnDateSetListener {
 
     public static final String TAG = "MainActivity";
-
-    public static final int ALLOW_INTERNET_DIALOG_CODE = 1;
-    public static final int NO_INTERNET_EXIT_CODE = 2;
-    public static final int ALLOW_INTERNET_STATUS_DIALOG_CODE = 3;
-    public static final int NO_INTERNET_STATUS_EXIT_CODE = 4;
-    public static final int ENABLE_INTERNET_DIALOG_CODE = 5;
-    public static final int RETRY_LOADING = 6;
-
-    public static final int ALLOW_INTERNET_REQUEST_CODE = 1270;
-    public static final int ALLOW_NETWORK_STATE_REQUEST_CODE = 1271;
 
     private ProgressDialog progressDialog = null;
     private ListFragment listFragment;
@@ -81,178 +65,18 @@ public class MainActivity extends AppCompatActivity implements DetailFragment.On
         SharedPreferences pf = PreferenceManager.getDefaultSharedPreferences(this);
         enhancedImage = pf.getBoolean("pref_enhanced", false);
 
-        checkForInternetPermission();
+        loadNasaData();
     }
 
     @Override
     public void onFragmentInteraction(Uri uri) {
     }
 
-    public void checkForInternetPermission() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
-            /* Need to provide an explanation */
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.INTERNET)) {
-                showDialog(getString(R.string.permission), getString(R.string.need_Internet_permission),
-                        android.R.drawable.ic_dialog_info, getString(R.string.allow), getString(R.string.deny), ALLOW_INTERNET_DIALOG_CODE);
-            } else {
-                requestInternetPermission();
-            }
-        } else {
-            checkNetworkStatePermission();
-        }
-    }
-
-    public void checkNetworkStatePermission() {
-        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_NETWORK_STATE) !=
-                PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.ACCESS_NETWORK_STATE)) {
-                showDialog(getString(R.string.permission), getString(R.string.need_nw_state_permissin),
-                        android.R.drawable.ic_dialog_info, getString(R.string.allow), getString(R.string.deny), ALLOW_INTERNET_STATUS_DIALOG_CODE);
-            } else {
-                requestNetworkStatusPermission();
-            }
-        } else {
-            loadNasaData();
-        }
-    }
-
-    public void showDialog(final String title, final String msg, final int icon, final String positiveStr, final String negativeStr,
-                           final int callbackCode) {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                new AlertDialog.Builder(MainActivity.this)
-                        .setTitle(title)
-                        .setMessage(msg)
-                        .setIcon(icon)
-                        .setPositiveButton(positiveStr, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialogCallback(callbackCode, true);
-                            }
-                        })
-                        .setNegativeButton(negativeStr, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialogCallback(callbackCode, false);
-                            }
-                        })
-                        .show();
-            }
-        });
-    }
-
-    public void dialogCallback(int code, boolean success) {
-        switch (code) {
-            case ALLOW_INTERNET_DIALOG_CODE:
-                if (success) {
-                    requestInternetPermission();
-                } else {
-                    showNoInternetExitDialog();
-                }
-                break;
-
-            case ALLOW_INTERNET_STATUS_DIALOG_CODE:
-                if (success) {
-                    requestNetworkStatusPermission();
-                } else {
-                    showNoNetworkStatusExitDialog();
-                }
-                break;
-
-            case NO_INTERNET_EXIT_CODE:
-                if (success) {
-                    checkForInternetPermission();
-                } else {
-                    System.exit(0);
-                }
-                break;
-
-            case NO_INTERNET_STATUS_EXIT_CODE:
-                if (success) {
-                    checkForInternetPermission();
-                } else {
-                    System.exit(0);
-                }
-                break;
-
-            case ENABLE_INTERNET_DIALOG_CODE:
-                if (success) {
-//                    Intent intent = new Intent(Intent.ACTION_MAIN);
-//                    intent.setClassName("com.android.phone", "com.android.phone.NetworkSetting");
-//                    startActivity(intent);
-
-                    startActivityForResult(new Intent(Settings.ACTION_WIFI_SETTINGS), 0);
-                    SystemClock.sleep(3000);
-                    loadNasaData();
-                } else {
-                    System.exit(0);
-                }
-                break;
-
-            case RETRY_LOADING:
-                if (success) {
-                    loadNasaData();
-                } else {
-                    System.exit(0);
-                }
-        }
-    }
-
-    public void showNoInternetExitDialog() {
-        showDialog(getString(R.string.permission), getString(R.string.no_proceed_without_Internet),
-                android.R.drawable.ic_dialog_alert, getString(R.string.allow), getString(R.string.exit), NO_INTERNET_EXIT_CODE);
-    }
-
-    public void showNoNetworkStatusExitDialog() {
-        showDialog(getString(R.string.permission), getString(R.string.no_proceed_without_nw_access),
-                android.R.drawable.ic_dialog_alert, getString(R.string.allow), getString(R.string.exit), NO_INTERNET_STATUS_EXIT_CODE);
-    }
-
-    public void requestInternetPermission() {
-        ActivityCompat.requestPermissions(MainActivity.this,
-                new String[]{Manifest.permission.INTERNET},
-                ALLOW_INTERNET_REQUEST_CODE);
-    }
-
-    public void requestNetworkStatusPermission() {
-        ActivityCompat.requestPermissions(MainActivity.this,
-                new String[]{Manifest.permission.ACCESS_NETWORK_STATE},
-                ALLOW_NETWORK_STATE_REQUEST_CODE);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case ALLOW_INTERNET_REQUEST_CODE:
-                if (grantResults.length > 0 &&
-                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    /* Permission granted for Internet access */
-                    checkNetworkStatePermission();
-                } else {
-                    showNoInternetExitDialog();
-                }
-                break;
-
-            case ALLOW_NETWORK_STATE_REQUEST_CODE:
-                if (grantResults.length > 0 &&
-                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    /* Permission Granted for Network state */
-
-                    loadNasaData();
-                } else {
-                    showNoNetworkStatusExitDialog();
-                }
-                break;
-        }
-    }
-
     public void loadNasaData() {
         Log.e(TAG, "loadNasaData");
 
         if (!isNetworkConnected()) {
-            showDialog(getString(R.string.Internet), getString(R.string.enable_Internet), android.R.drawable.ic_dialog_info,
-                    getString(R.string.enable), getString(R.string.cancel), ENABLE_INTERNET_DIALOG_CODE);
+            showDialog(getString(R.string.Internet), getString(R.string.enable_Internet), android.R.drawable.ic_dialog_info, getString(R.string.ok));
             return;
         }
 
@@ -294,9 +118,8 @@ public class MainActivity extends AppCompatActivity implements DetailFragment.On
                         Log.e(TAG, "Unexpected error");
                         dismissProgressDialog();
                         showDialog(getString(R.string.connectin), getString(R.string.failed_to_load),
-                                android.R.drawable.ic_dialog_alert, getString(R.string.retry), getString(R.string.exit), RETRY_LOADING);
+                                android.R.drawable.ic_dialog_alert, getString(R.string.ok));
                     } else {
-//                        Log.e(TAG, response.body().string());
                         loadCardData(response.body().string());
                         runOnUiThread(new Runnable() {
                             @Override
@@ -312,6 +135,36 @@ public class MainActivity extends AppCompatActivity implements DetailFragment.On
             Log.e(TAG, e + " 1 " + e.getMessage());
         }
     }
+
+    public void showDialog(final String title, final String msg, final int icon, final String positiveStr) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle(title)
+                        .setMessage(msg)
+                        .setIcon(icon)
+                        .setPositiveButton(positiveStr, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        })
+                        .show();
+            }
+        });
+    }
+
+//    public void dialogCallback(int code, boolean success) {
+//        switch (code) {
+//            case RETRY_LOADING:
+//                if (success) {
+//                    loadNasaData();
+//                } else {
+//                    System.exit(0);
+//                }
+//        }
+//    }
 
     public void loadCardData(String jsonStr) {
         try {
